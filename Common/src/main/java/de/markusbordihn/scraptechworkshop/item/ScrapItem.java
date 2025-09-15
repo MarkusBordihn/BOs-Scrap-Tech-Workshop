329045085784c4dcc80ff72c89e63c308c60a47c
Markus Bordihn <Markus@Bordihn.de>
2025-09-15 22:43:07 +0200

    Fixed automated tests.

--- Common/src/main/java/de/markusbordihn/scraptechworkshop/item/ScrapItem.java
+++ Common/src/main/java/de/markusbordihn/scraptechworkshop/item/ScrapItem.java
@@ -21,8 +21,6 @@ package de.markusbordihn.scraptechworkshop.item;
 
 import de.markusbordihn.scraptechworkshop.data.scrap.ScrapType;
 import java.util.List;
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -54,16 +52,13 @@ public abstract class ScrapItem extends Item {
   public abstract float getEfficiencyBonus();
 
   @Override
-  public Rarity getRarity(@Nonnull ItemStack stack) {
+  public Rarity getRarity(ItemStack stack) {
     return getScrapRarity();
   }
 
   @Override
   public void appendHoverText(
-      @Nonnull ItemStack stack,
-      @Nullable Level level,
-      @Nonnull List<Component> tooltip,
-      @Nonnull TooltipFlag flag) {
+      ItemStack stack, Level level, List<Component> tooltip, TooltipFlag flag) {
     super.appendHoverText(stack, level, tooltip, flag);
 
     tooltip.add(
